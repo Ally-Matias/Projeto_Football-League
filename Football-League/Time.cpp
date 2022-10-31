5fe96e29ad562c7fc8166ae8c5d7350475df611e
avelino <facoavelino9@gmail.com>
2022-10-31 20:57:21 -0300

    classe Time

---
+++ Football-League/Time.cpp
@@ -0,0 +1,37 @@
+//Implementação da classe Time.
+//includes.
+#include "Time.hpp"
+#include <iostream>
+using namespace std;
+
+//Implementação dos métodos.
+    Time::Time(string nome){
+        this->nome = nome;
+    }
+    Time::~Time(){}
+    string Time::getNome(){
+        return nome;
+    }
+    string Time::getResultados(){
+        cout<< vitorias <<endl;
+        cout<< derrotas <<endl;
+        cout<< empates <<endl;
+    }
+    string Time::imprimeJogadores(){
+        cout << Jogador::getNome << endl;
+    }
+    string Time::somaVitorias(){
+            if(vitorias > 0)
+            vitorias += 1;
+            cout << vitorias << endl;
+    }
+    string Time::somaDerrotas(){
+            if(derrotas > 0)
+            derrotas += 1;
+            cout << derrotas<< endl;
+    }
+    string Time::somaEmpates(){
+            if(empates > 0)
+            empates += 1;
+            cout << empates << endl;
+    }
---
+++ Football-League/Time.hpp
@@ -0,0 +1,27 @@
+//Prototipação da classe Time
+//Includes.
+#pragma once
+#include "Jogador.hpp";
+#include <iostream>
+using namespace std;
+
+//classe.
+class Time{
+    private:
+        string nome;
+        Jogador jogador;
+        int vitorias = 0;
+        int derrotas = 0;
+        int empates = 0;
+
+    public:
+        Time(string);
+        ~Time();
+        string getNome();
+        string getResultados();
+        string imprimeJogadores();
+        string somaVitorias();
+        string somaDerrotas();
+        string somaEmpates();
+        string insJogador(pJpg : Jogador);        
+};
