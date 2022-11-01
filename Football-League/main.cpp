4a85f8c5549f9af2db64d296d8eaae0e4049e46c
João Paulo Nobre Rodrigues <joaopaulonr.ti@gmail.com>
2022-11-01 12:34:56 -0300

    Driver de execução ( main.cpp ) criado.

---
+++ Football-League/main.cpp
@@ -0,0 +1,11 @@
+//Driver de execução.
+//includes.
+#include "Jogador.cpp"
+#include "jogadorGoleiro.cpp"
+#include "jogadorDefesa.cpp"
+#include "jogadorAtacante.cpp"
+#include "Time.cpp"
+//Função principal.
+int main(){
+    return 0;
+}
