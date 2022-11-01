99e2f2a926841a9bfe9a147da054dd0424e33c0d
João Paulo Nobre Rodrigues <joaopaulonr.ti@gmail.com>
2022-11-01 13:04:53 -0300

    Teste com vector < STL >

--- Football-League/main.cpp
+++ Football-League/main.cpp
@@ -5,7 +5,15 @@
 #include "jogadorDefesa.cpp"
 #include "jogadorAtacante.cpp"
 #include "Time.cpp"
+#include <vector>
+#include <iterator>
 //Função principal.
 int main(){
+    vector<jogadorDefesa>::iterator i;
+    vector<jogadorDefesa>jogadoresDefensivos;
+    jogadoresDefensivos.push_back(jogadorDefesa("Gabigol",31,89,128,9,76,60));
+    jogadoresDefensivos.push_back(jogadorDefesa("Neymar",32,95,400,10,89,99));
+    for(i=jogadoresDefensivos.begin();i!=jogadoresDefensivos.end();i++){
+        (*i).print();
     return 0;
 }
