25e8128daf2d965f937d119e51ed8b97a4ded709
João Paulo Nobre Rodrigues <joaopaulonr.ti@gmail.com>
2022-11-19 22:30:27 -0300

    List de jogadores concluído e adição de mais um menu.

--- Football-League/menus.cpp
+++ Football-League/menus.cpp
@@ -6,6 +6,7 @@ class menus{
     public:
         int static menuPrincipal();
         int static menuJogador();
+        int static menuListJogador();
         int static menuTime();
         int static menuPartida();
         int static menuDadosG();
@@ -75,3 +76,15 @@ int menus::menuDadosG(){ // ainda n implementado na main
     cin >> opcao;
     return opcao;
 }
+int menus::menuListJogador(){
+    cout << " " << endl;
+    cout << "----- LISTAR JOGADOR ----- " << endl;
+    cout << "Opção 1 - Listar Jogador atacante" << endl;
+    cout << "Opção 2 - Listar Jogador defensor" << endl;
+    cout << "Opção 3 - Listar Goleiro" << endl;
+    cout << "Opção 4 - Sair" << endl;
+    cout << " " << endl;
+    int opcao;
+    cin >> opcao;
+    return opcao;
+};
